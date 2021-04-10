e95143a0f7a5eead25ab79900bb2598da8dce102
Gianluca Pes <pesgianluca20@gmail.com>
2021-04-10 11:44:25 +0200

    initial commit

---
+++ src/main/java/com/xamon/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.xamon;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long>{
+
+	
+	
+}
---
+++ src/main/java/com/xamon/ProductService.java
@@ -0,0 +1,34 @@
+package com.xamon;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repo;
+	
+	public List<Product> listAll()
+	{
+		return repo.findAll();
+	}
+	public void save(Product product)
+	{
+		repo.save(product);
+	}
+	public Product get(Long id)
+	{
+		
+		return repo.findById(id).get();
+	}
+	
+	public void delete(Long id)
+	{
+		repo.deleteById(id);
+	}
+	
+}
